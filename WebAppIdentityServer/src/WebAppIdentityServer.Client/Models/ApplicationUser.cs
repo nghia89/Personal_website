8d83f18c0857f1caad40b39bdd5cf43656d76b8d
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2021-08-16 23:12:41 +0700

    remove folder

--- WebAppIdentityServer/src/WebAppIdentityServer.Client/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebAppIdentityServer.Client.Models
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
